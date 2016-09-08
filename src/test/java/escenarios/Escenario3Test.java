ec36cb0f8e1644f1a31ad807b563aa8c86a95e5c
Rubén Bresler Camps <ruben1981@gmail.com>
2016-09-08 12:50:21 -0500

    preparando test para ejecución completa de todos los test

--- src/test/java/escenarios/Escenario3Test.java
+++ src/test/java/escenarios/Escenario3Test.java
@@ -8,11 +8,7 @@ import static org.hamcrest.Matchers.notNullValue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 import implementacion.TipoMovimiento;
 import org.junit.Test;
@@ -30,7 +26,6 @@ import metricas.ObservadorVariablesEstado;
 
 public class Escenario3Test {
 
-    // @Test
     public void experimento1() throws InterruptedException {
 
         Escenarios e = Escenarios.builder()
@@ -132,108 +127,6 @@ public class Escenario3Test {
         }
     }
 
-    public void experimento(String name, Integer agentes, Double c1, Double c2, Double c3, TipoMovimiento movimiento, Object...movParams) throws InterruptedException {
-
-        System.out.println(name);
-        Escenarios e = Escenarios.builder()
-                .calcularMetricas(true)
-                .sensarEstado(false)
-                .guardarMetricasArchivo(true)
-                .mostrarSimulacion(false)
-                .iteraciones(2000)
-                .configuracionAgente(ConfiguracionAgente.builder()
-                        .cantidadAgentes(agentes)
-                        .cantidadObjetivos(1)
-                        .radioAgente(3)
-                        .rangoDeInteraccion(25)
-                        .radioObstaculos(20)
-                        .vecindad(new VecindadObjetivos())
-                        .build())
-                .configuracionModelo(ConfiguracionModelo.builder()
-                        .c1(c1)
-                        .c2(c2)
-                        .c3(c3)
-                        .zonaVirtual(15)
-                        .parametroObstaculos(1)
-                        .velMax(4)
-                        .maximaExtension(610)
-                        .maxPolarizacion(180)
-                        .build())
-                .ambiente(new AmbienteMovil())
-                .build();
-
-        assertThat(e, notNullValue());
-        assertThat(e.getConfiguracionAgente(), notNullValue());
-        assertThat(e.getConfiguracionAgente().getCantidadAgentes(), equalTo(agentes.doubleValue()));
-        assertThat(e.getConfiguracionAgente().getRadioAgente(), equalTo(3d));
-        assertThat(e.getConfiguracionModelo(), notNullValue());
-        assertThat(e.getConfiguracionModelo().getC1(), equalTo(c1));
-        assertThat(e.getConfiguracionModelo().getC3(), equalTo(c3));
-        assertThat(e.getConfiguracionModelo().getVelMax(), equalTo(4d));
-
-        for (int i = 0; i < e.getConfiguracionAgente().getCantidadObjetivos(); i++) {
-            AgenteMovilBuilder.builder()
-                    .withAmbiente(e.getAmbiente())
-                    .withX(300)
-                    .withY(100)
-                    .withConfiguracionAgente(e.getConfiguracionAgente())
-                    .withMovimiento(e.getConfiguracionModelo().buildMovimientoCuatroEsquinas())
-                    .buildObjetivo();
-        }
-
-        assertThat(e.getAmbiente(), notNullValue());
-        assertThat(e.getAmbiente().getAgentes(), hasSize(1));
-
-        Vector infIzq = new Vector(0, 0);
-        Vector supDer = new Vector(700, 700);
-
-        for (int i = 0; i < e.getConfiguracionAgente().getCantidadAgentes(); i++) {
-            Vector v = Util.aleatorioEnArea(infIzq, supDer);
-            AgenteMovilBuilder.builder()
-                    .withAmbiente(e.getAmbiente())
-                    .withX(v.get(0))
-                    .withY(v.get(1))
-                    .withMovimiento(e.getConfiguracionModelo().buildMovimiento(movimiento, movParams))
-                    .withConfiguracionAgente(e.getConfiguracionAgente())
-                    .buildBoid();
-        }
-
-        assertThat(e.getAmbiente().getAgentes(), hasSize(new Double(
-                e.getConfiguracionAgente().getCantidadAgentes() + e.getConfiguracionAgente().getCantidadObjetivos())
-                        .intValue()));
-
-        Simulacion s = Simulacion.builder()
-                .ambiente(e.getAmbiente())
-                .delaySimulacion(0)
-                .iteraciones(e.getIteraciones())
-                .observadores(e.getObservadores())
-                .simulacionActiva(true)
-                .build();
-
-        assertThat(e.getAmbiente().getAgentes(), hasSize(new Double(
-                e.getConfiguracionAgente().getCantidadAgentes() + e.getConfiguracionAgente().getCantidadObjetivos())
-                        .intValue()));
-        assertThat(s, notNullValue());
-        assertThat(s.getAmbiente().getAgentes(), hasSize(new Double(
-                e.getConfiguracionAgente().getCantidadAgentes() + e.getConfiguracionAgente().getCantidadObjetivos())
-                        .intValue()));
-
-        Thread t = new Thread(s);
-        t.start();
-        t.join();
-
-        ManejadorArchivos manejador = new ManejadorArchivos();
-        for (ObservadorAmbiente observer : s.getObservadores()) {
-            if (ObservadorMetricas.class.isAssignableFrom(observer.getClass())) {
-                manejador.guardarEnArchivo(name, e.getConfiguracionAgente().getAsMap(),
-                        e.getConfiguracionModelo().getAsMap(), (ObservadorMetricas) observer);
-            } else if (ObservadorVariablesEstado.class.isAssignableFrom(observer.getClass())) {
-                manejador.guardarEnArchivo(name, e.getConfiguracionAgente().getAsMap(),
-                        e.getConfiguracionModelo().getAsMap(), (ObservadorVariablesEstado) observer);
-            }
-        }
-    }
-
     @Test
     public void testAurelio() throws InterruptedException {
         List<Integer> cantidadAgentes = Arrays.asList(100, 150, 200, 250, 300, 350, 400, 450, 500);
@@ -244,38 +137,20 @@ public class Escenario3Test {
         Double c2 = .2;
         Double c3Min = -.1;
 
-        ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
-        List<Future<Void>> tasks = new ArrayList<>();
-
         for (int j = 0; j < 10; j++) {
             for (Integer agentes : cantidadAgentes) {
                 for (Double c1 : valoresC1) {
-                    // prueba con c3Min = -0.1
-                    tasks.add(pool.submit(new Experimento(agentes, c1, c2, c3Min, TipoMovimiento.MEJORADO, null)));
-                    // experimento(String.format("Prueba_%d_%1.2f_%1.2f_%1.2f", agentes.intValue(),
-                    // c1.doubleValue(), c2.doubleValue(), c3Min.doubleValue()), agentes, c1, c2, c3Min);
+                    new Experimento(agentes, c1, c2, c3Min, TipoMovimiento.MEJORADO, null).test();
                     for (Double c3 : valoresC3Max) {
-                        // prueba con valores de c3
-                        tasks.add(pool.submit(new Experimento(agentes, c1, c2, c3, TipoMovimiento.MEJORADO, null)));
-                        // experimento(String.format("Prueba_%d_%1.2f_%1.2f_%1.2f", agentes.intValue(),
-                        // c1.doubleValue(), c2.doubleValue(), c3.doubleValue()), agentes, c1, c2, c3);
+                        new Experimento(agentes, c1, c2, c3, TipoMovimiento.MEJORADO, null).test();
                     }
                 }
             }
         }
-
-        tasks.stream().forEach(t -> {
-            try {
-                t.get();
-            } catch (InterruptedException | ExecutionException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        });
     }
 
     @Test
-    public void testGustavo() throws InterruptedException {
+    public void testGustavo() throws InterruptedException, ExecutionException {
         List<Integer> cantidadAgentes = Arrays.asList(100, 150, 200, 250, 300, 350, 400, 450, 500);
         List<Double> valoresC1 = Arrays.asList(-.9, -.8, -.7, -.6, -.5, -.4, -.3, -.2, -.1, 0d, .1, .2, .3, .4, .5, .6,
                 .7, .8, .9);
@@ -284,28 +159,26 @@ public class Escenario3Test {
         Double c2 = .2;
         Double c3Min = -.1;
 
-        ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
-        List<Future<Void>> tasks = new ArrayList<>();
-
         for (int j = 0; j < 10; j++) {
             for (Integer agentes : cantidadAgentes) {
                 for (Double c1 : valoresC1) {
-                    tasks.add(pool.submit(new Experimento(agentes, c1, c2, c3Min, TipoMovimiento.INTEGRAL, new Object[]{0.002d})));
+                    new Experimento(agentes, c1, c2, c3Min, TipoMovimiento.INTEGRAL, new Object[]{0.01d}).test();
                     for (Double c3 : valoresC3Max) {
-                        tasks.add(pool.submit(new Experimento(agentes, c1, c2, c3, TipoMovimiento.INTEGRAL, new Object[]{0.002d})));
+                        new Experimento(agentes, c1, c2, c3, TipoMovimiento.INTEGRAL, new Object[]{0.01d}).test();
                     }
                 }
             }
         }
+    }
 
-        tasks.stream().forEach(t -> {
-            try {
-                t.get();
-            } catch (InterruptedException | ExecutionException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        });
+//    @Test
+    public void testManualSingle() throws InterruptedException, ExecutionException {
+        int cantidadAgentes = 500;
+        Double c1 = -.9;
+        Double c2 = .2;
+        Double c3 = -.1;
+
+        new Experimento(cantidadAgentes, c1, c2, c3, TipoMovimiento.INTEGRAL, new Object[]{0.01d}).test();
     }
 
     @AllArgsConstructor
@@ -318,9 +191,119 @@ public class Escenario3Test {
 
         @Override
         public Void call() throws Exception {
-            experimento(String.format("Prueba_%d_%1.2f_%1.2f_%1.2f", agentes.intValue(),
-                    c1.doubleValue(), c2.doubleValue(), c3.doubleValue()), agentes, c1, c2, c3, movimiento, movParams);
+            test();
             return null;
+        }
+
+        public void test() {
+            try {
+                running(String.format("Prueba_%d_%1.2f_%1.2f_%1.2f_%s", agentes.intValue(),
+                        c1.doubleValue(), c2.doubleValue(), c3.doubleValue(), movimiento.name()));
+            } catch (InterruptedException e) {
+                System.err.println("Error en test");
+            }
+        }
+
+        protected void running(String name) throws InterruptedException {
+
+            System.out.println(name);
+            Escenarios e = Escenarios.builder()
+                    .calcularMetricas(true)
+                    .sensarEstado(false)
+                    .guardarMetricasArchivo(true)
+                    .mostrarSimulacion(false)
+                    .iteraciones(2000)
+                    .configuracionAgente(ConfiguracionAgente.builder()
+                            .cantidadAgentes(agentes)
+                            .cantidadObjetivos(1)
+                            .radioAgente(3)
+                            .rangoDeInteraccion(25)
+                            .radioObstaculos(20)
+                            .vecindad(new VecindadObjetivos())
+                            .build())
+                    .configuracionModelo(ConfiguracionModelo.builder()
+                            .c1(c1)
+                            .c2(c2)
+                            .c3(c3)
+                            .zonaVirtual(15)
+                            .parametroObstaculos(1)
+                            .velMax(4)
+                            .maximaExtension(610)
+                            .maxPolarizacion(180)
+                            .build())
+                    .ambiente(new AmbienteMovil())
+                    .build();
+
+            assertThat(e, notNullValue());
+            assertThat(e.getConfiguracionAgente(), notNullValue());
+            assertThat(e.getConfiguracionAgente().getCantidadAgentes(), equalTo(agentes.doubleValue()));
+            assertThat(e.getConfiguracionAgente().getRadioAgente(), equalTo(3d));
+            assertThat(e.getConfiguracionModelo(), notNullValue());
+            assertThat(e.getConfiguracionModelo().getC1(), equalTo(c1));
+            assertThat(e.getConfiguracionModelo().getC3(), equalTo(c3));
+            assertThat(e.getConfiguracionModelo().getVelMax(), equalTo(4d));
+
+            for (int i = 0; i < e.getConfiguracionAgente().getCantidadObjetivos(); i++) {
+                AgenteMovilBuilder.builder()
+                        .withAmbiente(e.getAmbiente())
+                        .withX(300)
+                        .withY(100)
+                        .withConfiguracionAgente(e.getConfiguracionAgente())
+                        .withMovimiento(e.getConfiguracionModelo().buildMovimientoCuatroEsquinas())
+                        .buildObjetivo();
+            }
+
+            assertThat(e.getAmbiente(), notNullValue());
+            assertThat(e.getAmbiente().getAgentes(), hasSize(1));
+
+            Vector infIzq = new Vector(0, 0);
+            Vector supDer = new Vector(700, 700);
+
+            for (int i = 0; i < e.getConfiguracionAgente().getCantidadAgentes(); i++) {
+                Vector v = Util.aleatorioEnArea(infIzq, supDer);
+                AgenteMovilBuilder.builder()
+                        .withAmbiente(e.getAmbiente())
+                        .withX(v.get(0))
+                        .withY(v.get(1))
+                        .withMovimiento(e.getConfiguracionModelo().buildMovimiento(movimiento, movParams))
+                        .withConfiguracionAgente(e.getConfiguracionAgente())
+                        .buildBoid();
+            }
+
+            assertThat(e.getAmbiente().getAgentes(), hasSize(new Double(
+                    e.getConfiguracionAgente().getCantidadAgentes() + e.getConfiguracionAgente().getCantidadObjetivos())
+                    .intValue()));
+
+            Simulacion s = Simulacion.builder()
+                    .ambiente(e.getAmbiente())
+                    .delaySimulacion(0)
+                    .iteraciones(e.getIteraciones())
+                    .observadores(e.getObservadores())
+                    .simulacionActiva(true)
+                    .build();
+
+            assertThat(e.getAmbiente().getAgentes(), hasSize(new Double(
+                    e.getConfiguracionAgente().getCantidadAgentes() + e.getConfiguracionAgente().getCantidadObjetivos())
+                    .intValue()));
+            assertThat(s, notNullValue());
+            assertThat(s.getAmbiente().getAgentes(), hasSize(new Double(
+                    e.getConfiguracionAgente().getCantidadAgentes() + e.getConfiguracionAgente().getCantidadObjetivos())
+                    .intValue()));
+
+            Thread t = new Thread(s);
+            t.start();
+            t.join();
+
+            ManejadorArchivos manejador = new ManejadorArchivos();
+            for (ObservadorAmbiente observer : s.getObservadores()) {
+                if (ObservadorMetricas.class.isAssignableFrom(observer.getClass())) {
+                    manejador.guardarEnArchivo(name, e.getConfiguracionAgente().getAsMap(),
+                            e.getConfiguracionModelo().getAsMap(), (ObservadorMetricas) observer);
+                } else if (ObservadorVariablesEstado.class.isAssignableFrom(observer.getClass())) {
+                    manejador.guardarEnArchivo(name, e.getConfiguracionAgente().getAsMap(),
+                            e.getConfiguracionModelo().getAsMap(), (ObservadorVariablesEstado) observer);
+                }
+            }
         }
 
     }
